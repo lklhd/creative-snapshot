9493b8a85e09ac43ac72e88210fc0d5ca59065df
Aaron Dai <mrvicadai@gmail.com>
2016-04-18 15:00:50 -0700

    export CreativeSnapshot as one of the variables

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ export var ContainerNode = require('./components/ContainerNode/ContainerNode')
 export var ImageNode = require('./components/ImageNode/ImageNode')
 export var ShapeNode = require('./components/ShapeNode/ShapeNode')
 export var TextNode = require('./components/TextNode/TextNode')
-var CreativeSnapshot = require('./components/CreativeSnapshot/CreativeSnapshot')
-export default CreativeSnapshot
+export var CreativeSnapshot = require('./components/CreativeSnapshot/CreativeSnapshot')
+
