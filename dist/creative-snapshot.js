4a7ded193cf138d1961a204c3433473f66095e1c
Aaron Dai <mrvicadai@gmail.com>
2016-04-18 15:06:28 -0700

    Update dist

--- dist/creative-snapshot.js
+++ dist/creative-snapshot.js
@@ -288,10 +288,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 	
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -488,7 +484,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  data: _react2.default.PropTypes.array
 	};
 	
-	exports.default = CreativeSnapshot;
+	module.exports = CreativeSnapshot;
 
 /***/ },
 /* 7 */
